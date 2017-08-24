79d616447471e73df48b30959779e8f58d17a338
ackerapple <acker.dawn.apple@gmail.com>
2017-08-24 18:21:33 -0400

    fix ie

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
+exports.__esModule = true;
 var webcam_component_1 = require("./webcam.component");
 exports.WebCamComponent = webcam_component_1.WebCamComponent;
 //# sourceMappingURL=index.js.map
